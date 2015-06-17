ef5bb228251bb1abd2f8e6b6a9a1c6985a0dcc60
Ruben Garabaya <rubengarabaya@gmail.com>
2015-06-17 19:36:22 +0200

    seccion créditos

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Quiz'});
+});
+/* GET author page. */
+router.get('/author', function (req, res) {
+    res.render('author', {title: 'Autores',autor: 'Homero Simpson',video: "https://www.youtube.com/embed/eW9gt6g8ZAU"});
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
