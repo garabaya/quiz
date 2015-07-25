628ba41bd24d14946ceb9874e95463a7fe6cab46
Ruben Garabaya <rubengarabaya@gmail.com>
2015-07-25 19:49:31 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,11 @@ router.get('/author', function (req, res) {
     res.render('author', {title: 'Autores',autor: 'Homero Simpson',video: "https://www.youtube.com/embed/eW9gt6g8ZAU"});
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
